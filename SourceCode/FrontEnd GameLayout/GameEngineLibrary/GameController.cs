bdf906ef0fc616190a4c6758a2c32e7b44b67115
Morten Høgsberg <morten.h.f.k@gmail.com>
2022-03-28 08:51:00 +0200

    added controller

---
+++ SourceCode/FrontEnd GameLayout/GameEngineLibrary/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineLibrary
+{
+
+    public class GameController : Descriptor
+    {
+        public GameController() { }
+
+
+        public string Move(string direction)
+        {
+            return direction;
+        }
+    }
+}
