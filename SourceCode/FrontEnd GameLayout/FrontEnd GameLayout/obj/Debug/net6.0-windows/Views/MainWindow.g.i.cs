20309ebd61e02bebc30ac8dccc48eeae47b6e26a
magnusblaa <magnus.moeller@hotmail.com>
2022-04-02 10:32:58 +0200

    Fjernet backend mappe

--- SourceCode/FrontEnd GameLayout/FrontEnd GameLayout/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
+++ SourceCode/FrontEnd GameLayout/FrontEnd GameLayout/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
@@ -131,7 +131,7 @@ namespace FrontEnd_GameLayout.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FrontEnd GameLayout;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FrontEnd GameLayout;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
