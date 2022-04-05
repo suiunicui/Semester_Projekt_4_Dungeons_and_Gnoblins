d23250f6734ccb8ab116dc6d51d27b37b33c17b7
Morten Høgsberg <morten-hoegsberg@protonmail.com>
2022-04-05 12:13:05 +0200

    fixed problems with file directory

--- SourceCode/FrontEnd GameLayout/GameEngineLibrary/Map.cs
+++ SourceCode/FrontEnd GameLayout/GameEngineLibrary/Map.cs
@@ -1,28 +1,52 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Text;
+using System.Resources;
 using System.Threading.Tasks;
 
 namespace GameEngineLibrary
 {
-    public class Map
+  public class Map
+  {
+    public List<Room> Rooms { get; set; } = new List<Room>();
+    public LinkedList<int>[] MapLayout { get; set; }
+    private readonly string _mapLayoutResource = Environment.CurrentDirectory + "\\Resource.txt";
+
+    public Map(int numOfRooms)
     {
-        public List<Room> Rooms { get; set; }
-        public LinkedList<int>[] MapLayout { get; set; }
-        public int CurrentRoomId;
-
-        public Map()
-        {
-            int i = 0;
-            // missing file path
-            foreach(string line in System.IO.File.ReadLines())
-            {
-                string[] lines = line.Split(",");
-                int[] roomConnections = lines.Select(x => x == "" ? 0 : int.Parse(x)).ToArray();
-
-                MapLayout[i++] = new LinkedList<int>(roomConnections);
-            }
-        }
+      MapLayout = new LinkedList<int>[numOfRooms];
+      CreateConnectionsBetweenRoomsFromMapLayoutFile();
     }
+
+    private void CreateConnectionsBetweenRoomsFromMapLayoutFile()
+    {
+      ResourceReader reader = new ResourceReader(_mapLayoutResource);
+      CreateConnectionsBetweenRooms(reader);
+      reader.Close();
+    }
+
+    private void CreateConnectionsBetweenRooms(ResourceReader reader)
+    {
+      int index = 0;
+      foreach (DictionaryEntry entry in reader)
+      {
+        MapLayout[index++] = CreateConnection(entry);
+      }
+
+      MapLayout = MapLayout.Reverse().ToArray();
+    }
+
+    private LinkedList<int> CreateConnection(DictionaryEntry entry)
+    {
+      int[] connections = entry.Value.ToString()
+                                     .Split(",")
+                                     .Select(line => int.Parse(line))
+                                     .ToArray();
+      
+      return new LinkedList<int>(connections);
+    }
+  }
 }
--- SourceCode/FrontEnd GameLayout/GameEngineLibraryTest/TestMap.cs
+++ SourceCode/FrontEnd GameLayout/GameEngineLibraryTest/TestMap.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
 using GameEngineLibrary;
 using NUnit.Framework;
 
@@ -8,18 +12,44 @@ namespace GameEngineLibraryTest;
 public class TestMap
 {
   private Map _uut;
+  private string filepath = Environment.CurrentDirectory + "\\Resource.txt";
 
   [SetUp]
   public void Setup()
   {
-    _uut = new Map(10);
+    var filecreator = File.Create(filepath);
+    filecreator.Close();
+    ResourceWriter rw = new ResourceWriter(filepath);
+    rw.AddResource("room1","0,0,0,1");
+    rw.AddResource("room2","0,1,0,0");
+    rw.Close();
   }
 
   [Test]
-  public void MapCanReturnRoom()
+  public void MapConstructorWorks()
   {
-    int RoomId = 0;
+    _uut = new Map(2);
+    LinkedList<int> rooms = new LinkedList<int>();
+    rooms.AddLast(0);
+    rooms.AddLast(0);
+    rooms.AddLast(0);
+    rooms.AddLast(1);
 
-    Assert.That(_uut.GetRoom(RoomId), Is.Empty);
+    LinkedList<int> rooms2 = new LinkedList<int>();
+    rooms2.AddLast(0);
+    rooms2.AddLast(1);
+    rooms2.AddLast(0);
+    rooms2.AddLast(0);
+
+    Assert.That(_uut.MapLayout[0], Is.EqualTo(rooms));
+    Assert.That(_uut.MapLayout[1], Is.EqualTo(rooms2));
   }
+
+  [TearDown]
+  public void TearDown()
+  {
+    GC.Collect();
+    File.Delete(filepath);
+  }
+
 }
