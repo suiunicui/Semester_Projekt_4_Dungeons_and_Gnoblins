569160198d1fac16223bf7ae3f902f976c4d56bf
Morten Høgsberg <morten-hoegsberg@protonmail.com>
2022-04-10 20:28:21 +0200

    Added Log Class

--- SourceCode/FrontEnd GameLayout/GameEngineLibrary/Map.cs
+++ SourceCode/FrontEnd GameLayout/GameEngineLibrary/Map.cs
@@ -1,11 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.IO.Enumeration;
-using System.Linq;
-using System.Text;
 using System.Resources;
-using System.Threading.Tasks;
 
 namespace GameEngineLibrary
 {
