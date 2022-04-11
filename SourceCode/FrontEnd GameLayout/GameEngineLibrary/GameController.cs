a20b6aa8f3afcff508dff94cac1176d5cbe2f02d
Luyenvu6969 <79923251+Luyenvu6969@users.noreply.github.com>
2022-04-11 02:46:56 +0200

    Update GameController.cs

--- SourceCode/FrontEnd GameLayout/GameEngineLibrary/GameController.cs
+++ SourceCode/FrontEnd GameLayout/GameEngineLibrary/GameController.cs
@@ -17,5 +17,35 @@ namespace GameEngineLibrary
         {
             return direction;
         }
+         public string RoomDescription(Room Roomid, string Description)
+        {
+            //Få beskrivelse af Rum ud fra rummets id
+            string roomdescription = Description;
+
+            return roomdescription;
+        }
+        public void StartGame(Player player)
+        {
+        //Add map to game
+        Room.AddPlayer(Player player)
+        
+        }
+        //Overfører spiller til nyt rum
+        public void MovePlayer(Player player)
+        {
+        Room.AddPlayer(Player player)
+        
+        }
+        //fjerner spiller fra rummet han kom fra
+        public void PlayerMoved(Player player)
+        {
+        Room.Player RemovePlayer()
+        }
+        //Fjerner spiller fra spillet og viser death screen.
+        public void PlayerDead(Player player)
+        {
+        Room.Player RemovePlayer()
+        //Display Death screen
+        }
     }
 }
