01a0c80eda9a3bcc18e83c2d276eb5383ce55115
hundahl18 <hundahl18@gmail.com>
2022-04-14 19:27:55 +0200

    Changed Frontend to use Gamelogic again

--- SourceCode/FrontEnd GameLayout/FrontEnd GameLayout/obj/Debug/net6.0-windows/Views/Room.g.cs
+++ SourceCode/FrontEnd GameLayout/FrontEnd GameLayout/obj/Debug/net6.0-windows/Views/Room.g.cs
@@ -309,7 +309,7 @@ namespace FrontEnd_GameLayout.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -325,7 +325,7 @@ namespace FrontEnd_GameLayout.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
