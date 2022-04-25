227acdf866a08dc48034d1faa61ccf70ffd5912f
magnusblaa <magnus.moeller@hotmail.com>
2022-04-25 11:13:47 +0200

    fixed errors

---
+++ BackendCode/Backend_API/Backend_API/Migrations/20220413135110_addedUsers.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_API.Migrations
+{
+    public partial class addedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Saves",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Saves_UserId",
+                table: "Saves",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Saves_Users_UserId",
+                table: "Saves",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Saves_Users_UserId",
+                table: "Saves");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Saves_UserId",
+                table: "Saves");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Saves");
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                });
+        }
+    }
+}
---
+++ BackendCode/Backend_API/Backend_API/Models/Save.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Backend_API.Models
+{
+    public class Save
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        public int RoomID { get; set; }
+
+        public string SaveName { get; set; }
+
+
+
+    }
+}
