77e80b61935521352c3df52da915e9ae49fb2559
hundahl18 <hundahl18@gmail.com>
2022-05-05 09:07:38 +0200

    Starting to move backend to secondary solution

---
+++ SourceCode/FrontEnd GameLayout/GameEngine/Models/DTO/SaveDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_API.Models.DTO
+{
+    public class SaveDTO
+    {
+        public int ID { get; set; }
+
+        public int RoomId { get; set; }
+
+        public string SaveName { get; set; }
+    }
+}
