96d00c986a81940f08fcdf0147b2ab0f47d1d93b
magnusblaa <magnus.moeller@hotmail.com>
2022-05-24 13:52:08 +0200

    small fixes

--- SourceCode/FrontEnd GameLayout/Backend_API/DAL/DAL.cs
+++ SourceCode/FrontEnd GameLayout/Backend_API/DAL/DAL.cs
@@ -416,9 +416,9 @@ namespace Backend_API.DAL
         {
             
 
-            var rooms = await _context.Saves.ToListAsync();
+            var saves = await _context.Saves.ToListAsync();
 
-            return rooms;
+            return saves;
 
         }
 
